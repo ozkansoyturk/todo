cae710a38863e8c7f6534640196862a28a1c29a0
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-13 01:07:10 +0100

    Ajouter une todo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import "./style.css";
 
 const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
 
-const todos = [
-  {
-    text: "je suis une todo",
-    done: false,
-  },
-  {
-    text: "faire du javascript",
-    done: false,
-  },
-];
+const todos = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  if (value !== "") {
+    addTodo(value);
+  } else {
+    alert("il faut entrer des mots");
+  }
+  displayTodo();
+});
 
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
@@ -27,9 +32,18 @@ const createTodoElement = (todo, index) => {
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>
-  `;
+    <button class="modify">modifier</button>
+    <button class="delete">supprimer</button>
+    `;
   console.log(li);
   return li;
+};
+
+const addTodo = (text) => {
+  todos.unshift({
+    text,
+    done: false,
+  });
 };
 
 displayTodo();
