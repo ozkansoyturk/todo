1b31d35e346325b322feb4be63fe63d22e27933c
ozkansoyturk <soyturkozkan@gmail.com>
2023-03-14 02:22:59 +0100

    Suppression d'une todo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const ul = document.querySelector("ul");
 const form = document.querySelector("form");
 const input = document.querySelector("form > input");
 
-const todos = [];
+const todos = [{ text: "je suis une todo", done: false }];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -22,20 +22,24 @@ const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
     return createTodoElement(todo, index);
   });
-  console.log(todosNode);
   ul.innerHTML = "";
   ul.append(...todosNode);
 };
 
 const createTodoElement = (todo, index) => {
   const li = document.createElement("li");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "Supprimer";
+  buttonDelete.className = "delete";
+  buttonDelete.addEventListener("click", (event) => {
+    deleteTodo();
+  });
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>
     <button class="modify">modifier</button>
-    <button class="delete">supprimer</button>
     `;
-  console.log(li);
+  li.appendChild(buttonDelete);
   return li;
 };
 
@@ -46,4 +50,8 @@ const addTodo = (text) => {
   });
 };
 
+const deleteTodo = (index) => {
+  todos.splice(index, 1);
+  displayTodo();
+};
 displayTodo();
